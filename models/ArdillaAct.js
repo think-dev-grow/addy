b838675405cb11098e832ddabafd62b718c8df0c
overlord <rexatuzie@gmail.com>
2022-12-06 16:37:15 +0100

    tue commit 43

--- models/ArdillaAct.js
+++ models/ArdillaAct.js
@@ -3,9 +3,9 @@ const mongoose = require("mongoose");
 const ArdillaAccountSchema = new mongoose.Schema(
   {
     userID: { type: String },
-    sanBalance: { type: Number, default: 0.06 },
-    dillaWallet: { type: Number, default: 0.0 },
-    totalFunds: { type: Number, default: 0.0 },
+    sanBalance: { type: Number, default: "0.00" },
+    dillaWallet: { type: Number, default: "0.00" },
+    totalFunds: { type: Number, default: "0.00" },
     kycPoints: { type: Number, default: 0 },
     badge: { type: String, default: "cadet" },
   },
