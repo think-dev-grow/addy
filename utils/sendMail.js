9844d16e07047a801b9d99b4f124cee316036881
overlord <rexatuzie@gmail.com>
2022-12-11 17:42:18 +0100

    sun commit 3

--- utils/sendMail.js
+++ utils/sendMail.js
@@ -39,7 +39,55 @@ const sendVerificationMail = (to, value) => {
         },
       ],
       subject: "Email verifaction",
-      htmlbody: `<table
+      htmlbody: `<body style="margin: 0;">
+    
+      <table
+        cellSpacing="0"
+        cellPadding="0"
+        style="background-image: url(https://i.postimg.cc/g0B05pW5/background-6.png); border: 1px solid #eee; width: 100%; padding-bottom: 25px;">
+        <tbody>
+          <tr>
+            <td>
+            
+              <div style="background-image: url(https://i.postimg.cc/pXgHF8bN/Background-2.png); border: 1px solid #eee; box-sizing: border-box; font-family: 'ubuntu',sans-serif; padding: 90px 50px; margin: 40px auto; max-width: 600px;  width: 600px;">
+                <div style="display: flex; align-items: center; padding-bottom: 190px; margin-bottom: 30px;"> 
+               
+                  <div class="text-center" style="height: 493.78570556640625px; width: 633px; margin-top: -75px;">
+                   
+                    <img style="width: 104.06px;height: 38.77px; margin-top: 70px;" src="https://i.postimg.cc/NjcpQcYP/Logo-1.png" alt="Logo">
+                    <hr style="color: gray/300;margin-top: 39.09px;border: 1px solid black">
+                    <img style="margin-top: 40px;" src="https://i.postimg.cc/GtXGRFmK/Mask-group.png" alt="illustration">
+                    <h3 style="margin-top: 25.64px;font-family: 'ubuntu',sans-serif;font-size:20px;color: #4B5563; font-weight:600;font-style: normal;">Hi ${name},</h3>
+                    <p style="font-size: 16px;font-weight: 500;font-family: 'ubuntu',sans-serif;text-align: center;color:#4B5563; margin-top: 41px;">Welcome to Ardilla! Your number one wealth-building platform.<br>
+  
+                      <br>At Ardilla, we did the hard work, so you don’t have to because we believe wealth building should be easy for everyone.<br>
+                      
+                      <br>Continue your wealth-building journey on our app. We have got you covered.
+                   </p>
+                    <p style="color: #4B5563;font-family: 'ubuntu',sans-serif;font-style: normal;font-size: 16px;font-weight: 500;margin-top: 30px;"">Have Fun</p>
+                    <p style="color: #4B5563;height: 26px;font-size: 14.2px;font-weight: 600;line-height: 26px; margin-top: 28px;">The Ardilla Team</p>
+                    <p style="font-size: 12.86px;color: #6B7280;font-weight: 500;font-family: 'ubunt',sans-serif;height: 25px;font-style: normal; margin-top: 20px;">Copyright © 2022 Ardilla. All rights reserved.</p>
+  
+   
+                   
+                   
+     
+                  </div>
+  
+                
+                </div>
+                </div>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+  </body>`,
+    })
+    .then((resp) => console.log("success", resp))
+    .catch((error) => console.log("error", error));
+};
+{
+  /* <table
       cellSpacing="0"
       cellPadding="0"
       style="background-image: url(https://i.postimg.cc/g0B05pW5/background-6.png); border: 1px solid #eee; width: 100%;"
@@ -77,12 +125,7 @@ const sendVerificationMail = (to, value) => {
               </td>
             </tr>
           </tbody>
-        </table>`,
-    })
-    .then((resp) => console.log("success", resp))
-    .catch((error) => console.log("error", error));
-};
-
+        </table>
 //Reset password mail
 const resetPassword = (to, name, username, token) => {
   client
@@ -143,7 +186,8 @@ const resetPassword = (to, name, username, token) => {
     })
     .then((resp) => console.log("success", resp))
     .catch((error) => console.log("error", error));
-};
+}; */
+}
 
 //complete profile mail
 const sendCompleteProfile = (to, name) => {
@@ -163,46 +207,49 @@ const sendCompleteProfile = (to, name) => {
         },
       ],
       subject: "Profile completeion",
-      htmlbody: `<table
-      cellSpacing="0"
-      cellPadding="0"
-      style="background-image: url(https://i.postimg.cc/g0B05pW5/background-6.png); border: 1px solid #eee; width: 100%; padding-bottom: 25px;">
-      <tbody>
-        <tr>
-          <td>
-          
-            <div style="background-image: url(https://i.postimg.cc/pXgHF8bN/Background-2.png); border: 1px solid #eee; box-sizing: border-box; font-family: 'ubuntu',sans-serif; padding: 90px 50px; margin: 40px auto; max-width: 600px;  width: 600px;">
-              <div style="display: flex; align-items: center; padding-bottom: 190px; margin-bottom: 30px;"> 
-             
-                <div class="text-center" style="height: 493.78570556640625px; width: 633px; margin-top: -75px;">
-                 
-                  <img style="width: 104.06px;height: 38.77px; margin-top: 70px;" src="https://i.postimg.cc/NjcpQcYP/Logo-1.png" alt="Logo">
-                  <hr style="color: gray/300;margin-top: 39.09px;border: 1px solid black">
-                  <img style="margin-top: 40px;" src="https://i.postimg.cc/GtXGRFmK/Mask-group.png" alt="illustration">
-                  <h3 style="margin-top: 25.64px;font-family: 'ubuntu',sans-serif;font-size:20px;color: #4B5563; font-weight:600;font-style: normal;">Hi ${name},</h3>
-                  <p style="font-size: 16px;font-weight: 500;font-family: 'ubuntu',sans-serif;text-align: center;color:#4B5563; margin-top: 41px;">Welcome to Ardilla! Your number one wealth-building platform.<br>
-
-                    <br>At Ardilla, we did the hard work, so you don’t have to because we believe wealth building should be easy for everyone.<br>
-                    
-                    <br>Continue your wealth-building journey on our app. We have got you covered.
-                 </p>
-                  <p style="color: #4B5563;font-family: 'ubuntu',sans-serif;font-style: normal;font-size: 16px;font-weight: 500;margin-top: 30px;"">Have Fun</p>
-                  <p style="color: #4B5563;height: 26px;font-size: 14.2px;font-weight: 600;line-height: 26px; margin-top: 28px;">The Ardilla Team</p>
-                  <p style="font-size: 12.86px;color: #6B7280;font-weight: 500;font-family: 'ubunt',sans-serif;height: 25px;font-style: normal; margin-top: 20px;">Copyright © 2022 Ardilla. All rights reserved.</p>
-
- 
-                 
-                 
-   
-                </div>
-
+      htmlbody: `<body style="margin: 0;">
+    
+        <table
+          cellSpacing="0"
+          cellPadding="0"
+          style="background-image: url(https://i.postimg.cc/g0B05pW5/background-6.png); border: 1px solid #eee; width: 100%; padding-bottom: 25px;">
+          <tbody>
+            <tr>
+              <td>
               
-              </div>
-              </div>
-          </td>
-        </tr>
-      </tbody>
-    </table>`,
+                <div style="background-image: url(https://i.postimg.cc/pXgHF8bN/Background-2.png); border: 1px solid #eee; box-sizing: border-box; font-family: 'ubuntu',sans-serif; padding: 90px 50px; margin: 40px auto; max-width: 600px;  width: 600px;">
+                  <div style="display: flex; align-items: center; padding-bottom: 190px; margin-bottom: 30px;"> 
+                 
+                    <div class="text-center" style="height: 493.78570556640625px; width: 633px; margin-top: -75px;">
+                     
+                      <img style="width: 104.06px;height: 38.77px; margin-top: 70px;" src="https://i.postimg.cc/NjcpQcYP/Logo-1.png" alt="Logo">
+                      <hr style="color: gray/300;margin-top: 39.09px;border: 1px solid black">
+                      <img style="margin-top: 40px;" src="https://i.postimg.cc/GtXGRFmK/Mask-group.png" alt="illustration">
+                      <h3 style="margin-top: 25.64px;font-family: 'ubuntu',sans-serif;font-size:20px;color: #4B5563; font-weight:600;font-style: normal;">Hi ${name},</h3>
+                      <p style="font-size: 16px;font-weight: 500;font-family: 'ubuntu',sans-serif;text-align: center;color:#4B5563; margin-top: 41px;">Welcome to Ardilla! Your number one wealth-building platform.<br>
+    
+                        <br>At Ardilla, we did the hard work, so you don’t have to because we believe wealth building should be easy for everyone.<br>
+                        
+                        <br>Continue your wealth-building journey on our app. We have got you covered.
+                     </p>
+                      <p style="color: #4B5563;font-family: 'ubuntu',sans-serif;font-style: normal;font-size: 16px;font-weight: 500;margin-top: 30px;"">Have Fun</p>
+                      <p style="color: #4B5563;height: 26px;font-size: 14.2px;font-weight: 600;line-height: 26px; margin-top: 28px;">The Ardilla Team</p>
+                      <p style="font-size: 12.86px;color: #6B7280;font-weight: 500;font-family: 'ubunt',sans-serif;height: 25px;font-style: normal; margin-top: 20px;">Copyright © 2022 Ardilla. All rights reserved.</p>
+    
+     
+                     
+                     
+       
+                    </div>
+    
+                  
+                  </div>
+                  </div>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+    </body>`,
     })
     .then((resp) => console.log("success", resp))
     .catch((error) => console.log("error", error));
