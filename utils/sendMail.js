d9448fb89a0ca88073f85b547fa6c8603b3ac9a3
overlord <rexatuzie@gmail.com>
2022-12-12 03:09:50 +0100

    mon commit 23456

--- utils/sendMail.js
+++ utils/sendMail.js
@@ -60,61 +60,30 @@ const sendVerificationMail = (to, value) => {
         },
       ],
       subject: "Email verifaction",
-      htmlbody: `<body style="margin: 0;">
-    
-      <table
-        cellSpacing="0"
-        cellPadding="0"
-        style="background-image: url(https://i.postimg.cc/g0B05pW5/background-6.png); border: 1px solid #eee; width: 100%; padding-bottom: 25px;">
-        <tbody>
+      htmlbody: `
+      <table cellspacing="0" cellpadding="0" style="background-color: #F4F3ED; border: 1px solid #eee; width: 100%;">
+      <tbody>
           <tr>
-            <td>
-            
-              <div style="background-image: url(https://i.postimg.cc/pXgHF8bN/Background-2.png); border: 1px solid #eee; box-sizing: border-box; font-family: 'ubuntu',sans-serif; padding: 40px 50px; margin: 40px auto; max-width: 600px;  width: 600px;">
+              <td>
+                  <div style="background-color: #fff;  box-shadow: 0px 9px 12px #654C1A27;  box-sizing: border-box; font-family: Lato, Helvetica, 'Helvetica Neue', Arial, 'sans-serif'; margin: auto; max-width: 600px; overflow: hidden; width: 600px;">
+                          <div style="">
+                              
+                             
+                              <div style="padding: 0 80px 50px;">
+                                  <p style="font-size: 14px; margin: 0; line-height: 22px;">Please use the OTP code below to complete your account setup.</p>
+                                 
 
-               
-
-                <div style="display: flex; align-items: center; padding-bottom: 20px; margin-bottom: 30px;">
-               
-                  
-                  <div style="text-align: center;">
-
-                 
-                   
-                    <img style="width: 104.06px;height: 38.77px; " src="https://i.postimg.cc/NjcpQcYP/Logo-1.png" alt="Logo">
-
-                    <hr style="color: gray/300; margin-top: 39.09px;">
-
-                    <img style="margin-top: 40px;" src="https://i.postimg.cc/GtXGRFmK/Mask-group.png" alt="illustration">
-
-
-                    <p style="font-size: 16px;font-weight: 500;font-family: 'ubuntu',sans-serif;text-align: center;color:#4B5563; margin-top: 41px;">Please use the OTP code below to complete your account setup.</p>
-
-                  
-
-
-                  <p style="color:#383636; font-size: 38px; font-weight: 700; font-family: 'Ubuntu'; margin-top: 20px;">${value}</p> 
-                   
-                    
-                    <p style="color: #4B5563;font-family: 'ubuntu',sans-serif;font-style: normal;font-size: 16px;font-weight: 500;margin-top: 30px;"">Have Fun</p>
-                    <p style="color: #4B5563; font-size: 14.2px; font-weight: 600; margin-top: 28px;">The Ardilla Team</p>
-
-                    <p style="font-size: 12.86px; color: #6B7280; font-weight: 500; font-family: 'ubunt',sans-serif; font-style: normal; margin-top: 20px;">Copyright © 2022 Ardilla. All rights reserved.</p>
-  
-   
-                   
-                   
-     
-                  </div>
-  
-                
-                </div>
-                </div>
-            </td>
+                                 < hr style="margin-top: 39px;">
+                                 
+                                <h3style="font-size: 30px; font-weight: 600; margin: 0; line-height: 22px; color:#383636;">${value}</h3style=>
+                              </div>
+                          </div>
+                      </div>
+               </td>
           </tr>
-        </tbody>
-      </table>
-  </body>`,
+      </tbody>
+  </table>   
+ `,
     })
     .then((resp) => console.log("success", resp))
     .catch((error) => console.log("error", error));
