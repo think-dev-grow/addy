f855af3cfe4a45977841722eed2a426d82de1a90
backend dev <developer@leapsail.com.ng>
2023-01-04 16:41:42 +0100

    wed commit 123

--- controllers/user.js
+++ controllers/user.js
@@ -1,6 +1,13 @@
 const User = require("../models/User");
 const jwt = require("jsonwebtoken");
 const handleError = require("../utils/error");
+const cloudinary = require("cloudinary").v2;
+
+cloudinary.config({
+  cloud_name: "domthgc9v",
+  api_key: "382674119878141",
+  api_secret: "FAupYcLySkHxYuVyF5J36UTy2y0",
+});
 
 const getUserById = async (req, res, next) => {
   try {
@@ -37,11 +44,26 @@ const getUser = async (req, res, next) => {
   }
 };
 
-const profileImage = async (req, res, next) => {
+const profileImage = async () => {
+  const { image } = req.body;
+
+  const uploadImg = await cloudinary.uploader.upload(
+    image,
+    {
+      upload_preset: "ifaoski1",
+      public_id: `${Date.now()}`,
+      allowed_formats: ["png", "jpg", "jpeg", "svg", "ico", "jfif", "webp"],
+    },
+    function (error, result) {
+      if (error) {
+        console.log(error);
+      }
+      console.log(result);
+    }
+  );
+
   try {
     const id = req.params.id;
-
-    const { profilePic } = req.body;
 
     const user = await User.findById(id);
 
@@ -50,7 +72,7 @@ const profileImage = async (req, res, next) => {
     await User.findOneAndUpdate(
       { _id: id },
       {
-        $set: { profilePic: profilePic },
+        $set: { profilePic: uploadImg.url },
       },
       { new: true }
     );
@@ -60,5 +82,29 @@ const profileImage = async (req, res, next) => {
     next(error);
   }
 };
+
+// const profileImage = async (req, res, next) => {
+//   try {
+//     const id = req.params.id;
+
+//     const { profilePic } = req.body;
+
+//     const user = await User.findById(id);
+
+//     if (!user) return next(handleError(400, "user does not exist"));
+
+//     await User.findOneAndUpdate(
+//       { _id: id },
+//       {
+//         $set: { profilePic: profilePic },
+//       },
+//       { new: true }
+//     );
+
+//     res.status(200).json({ success: true, msg: "profile pic uploaded" });
+//   } catch (error) {
+//     next(error);
+//   }
+// };
 
 module.exports = { getUser, getUserById, profileImage };
