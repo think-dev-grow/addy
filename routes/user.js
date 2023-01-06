5814fc2e3af6927026acf36b641f1cc450d040cc
backend dev <developer@leapsail.com.ng>
2023-01-06 12:53:34 +0100

    gdgs

--- routes/user.js
+++ routes/user.js
@@ -11,7 +11,7 @@ const { upload } = require("../utils/uploadFile");
 
 router.get("/getUser/:token", getUser);
 router.get("/find/:id", getUserById);
-router.post("/profile-pic", upload.single("image"), profileImage);
+router.post("/profile-pic/:id", upload.single("image"), profileImage);
 router.put("/next-of-kin/:id", nextOfKin);
 
 module.exports = router;
