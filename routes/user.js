4ea965c6f6ade575b865cb8f3931d6a6f08ac9ac
overlord <rexatuzie@gmail.com>
2023-01-08 21:17:04 +0100

    nnbdc,nfv

--- routes/user.js
+++ routes/user.js
@@ -7,6 +7,8 @@ const {
   uploadIdFront,
   uploadIdBack,
   uploadUtilityBill,
+  decline,
+  approve,
 } = require("../controllers/user");
 const router = express.Router();
 
@@ -16,6 +18,8 @@ router.get("/getUser/:token", getUser);
 router.get("/find/:id", getUserById);
 router.post("/profile-pic/:id", upload.single("image"), profileImage);
 router.put("/next-of-kin/:id", nextOfKin);
+router.put("/decline/:id", decline);
+router.put("/approve/:id", approve);
 router.post("/id-front/:id", upload.single("image"), uploadIdFront);
 router.post("/id-back/:id", upload.single("image"), uploadIdBack);
 router.post("/utility-bill/:id", upload.single("image"), uploadUtilityBill);
