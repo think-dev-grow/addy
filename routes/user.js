3c921dd0fa25b42ebf4f10884b94f53d8ba09724
overlord <rexatuzie@gmail.com>
2023-01-09 03:43:43 +0100

    tghhyg

--- routes/user.js
+++ routes/user.js
@@ -7,8 +7,13 @@ const {
   uploadIdFront,
   uploadIdBack,
   uploadUtilityBill,
-  decline,
-  approve,
+  declineUtilityBill,
+  approveUtility,
+  declineIdFront,
+  approveIdFront,
+  declineIdBack,
+  approveIdBack,
+  genrateAccount,
 } = require("../controllers/user");
 const router = express.Router();
 
@@ -18,8 +23,13 @@ router.get("/getUser/:token", getUser);
 router.get("/find/:id", getUserById);
 router.post("/profile-pic/:id", upload.single("image"), profileImage);
 router.put("/next-of-kin/:id", nextOfKin);
-router.put("/decline/:id", decline);
-router.put("/approve/:id", approve);
+router.put("/decline-utility/:id", declineUtilityBill);
+router.put("/approve-utility/:id", approveUtility);
+router.put("/decline-front/:id", declineIdFront);
+router.put("/approve-front/:id", approveIdFront);
+router.put("/decline-back/:id", declineIdBack);
+router.put("/approve-back/:id", approveIdBack);
+router.put("/generate-san/:id", genrateAccount);
 router.post("/id-front/:id", upload.single("image"), uploadIdFront);
 router.post("/id-back/:id", upload.single("image"), uploadIdBack);
 router.post("/utility-bill/:id", upload.single("image"), uploadUtilityBill);
